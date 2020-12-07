96a7756a156c1567c800abacf20b2df4ec72655d
André Martinez <andremartinez92@gmail.com>
2020-12-07 11:12:59 +0000

    Improve actions tests

--- src/middleware/__tests__/actions.spec.js
+++ src/middleware/__tests__/actions.spec.js
@@ -1,6 +1,9 @@
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk';
 import { ROBOTS } from '../../testHelpers/RobotsTestHelpers';
-import { changeSearchField, getRobots } from '../actions';
 import { CHANGE_SEARCH_FIELD_TYPE, GET_ROBOTS_TYPES } from '../constants';
+
+import { changeSearchField, getRobots } from '../actions';
 
 describe('#changeSearchField', () => {
   it('should create an action to change search field', () => {
@@ -15,46 +18,39 @@ describe('#changeSearchField', () => {
 });
 
 describe('#getRobots', () => {
-  const dispatch = jest.fn();
+  let mockedStore;
   beforeEach(() => {
-    dispatch.mockClear();
     fetch.resetMocks();
+    mockedStore = configureMockStore([thunk])(undefined);
   });
 
-  describe('returned function', () => {
-    it('dispatches the REQUEST action on call', async () => {
-      const actionDispatchFn = getRobots();
-      await actionDispatchFn(dispatch);
-      expect(dispatch).toHaveBeenCalledWith(
-        { type: GET_ROBOTS_TYPES.REQUEST, payload: {} }
-      )
+
+  it('dispatches success after fetching', () => {
+    fetch.mockResponse(JSON.stringify(ROBOTS))
+
+    const expectedActions = [
+      { type: GET_ROBOTS_TYPES.REQUEST, payload: {} },
+      { type: GET_ROBOTS_TYPES.SUCCESS, payload: { robots: ROBOTS } }
+    ];
+
+    expect.assertions(1);
+    return mockedStore.dispatch(getRobots()).then(() => {
+      expect(mockedStore.getActions()).toEqual(expectedActions)
     })
+  })
 
-    it('dispatches the SUCCESS action on call', async () => {
-      fetch.mockResponse(JSON.stringify(ROBOTS))
-      const actionDispatchFn = getRobots();
-      await actionDispatchFn(dispatch);
+  it('dispatches failure after rejection', () => {
+    const error = new Error('some error');
+    fetch.mockReject(error)
 
-      expect(dispatch.mock.calls).toEqual(
-        [
-          [{ type: GET_ROBOTS_TYPES.REQUEST, payload: {} }],
-          [{ type: GET_ROBOTS_TYPES.SUCCESS, payload: { robots: ROBOTS } }]
-        ]
-      )
-    })
+    const expectedActions = [
+      { type: GET_ROBOTS_TYPES.REQUEST, payload: {} },
+      { type: GET_ROBOTS_TYPES.FAILURE, payload: { error } }
+    ];
 
-    it('dispatches the FAILURE action on rejection', async () => {
-      const error = new Error('some error');
-      fetch.mockReject(error);
-      const actionDispatchFn = getRobots();
-      await actionDispatchFn(dispatch);
-
-      expect(dispatch.mock.calls).toEqual(
-        [
-          [{ type: GET_ROBOTS_TYPES.REQUEST, payload: {} }],
-          [{ type: GET_ROBOTS_TYPES.FAILURE, payload: { error } }]
-        ]
-      )
+    expect.assertions(1);
+    return mockedStore.dispatch(getRobots()).then(() => {
+      expect(mockedStore.getActions()).toEqual(expectedActions)
     })
   })
 })
